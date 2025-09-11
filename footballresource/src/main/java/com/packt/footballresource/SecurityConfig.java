169ad6c8874b229540428ae6a7341beb02950b45
Bram Eldering <bram.eldering@gmail.com>
2025-09-11 11:57:54 +0200

    Added security validation on role

--- footballresource/src/main/java/com/packt/footballresource/SecurityConfig.java
+++ footballresource/src/main/java/com/packt/footballresource/SecurityConfig.java
@@ -14,4 +14,14 @@ public class SecurityConfig {
     return http.authorizeHttpRequests(authorize -> authorize.requestMatchers(HttpMethod.GET, "/football/teams/**").hasAuthority("SCOPE_football:read").requestMatchers(HttpMethod.POST, "/football/teams/**").hasAuthority("SCOPE_football:admin").anyRequest().authenticated())
         .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).build();
   }
+
+  @Bean
+  public SecurityFilterChain filterChainRoles(HttpSecurity http) throws Exception {
+    return http
+        .authorizeHttpRequests(
+            authorize -> authorize.requestMatchers(HttpMethod.POST, "football/teams/**").hasRole("ADMIN")
+                .anyRequest().authenticated())
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+        .build();
+  }
 }
