d541c9952edec41c35124ee3bea146d3f4269ce7
Bram Eldering <bram.eldering@gmail.com>
2025-09-11 19:13:20 +0200

    Logging in with Google Accounts OpenID

--- footballui/src/main/java/com/packt/footballui/SecurityConfiguration.java
+++ footballui/src/main/java/com/packt/footballui/SecurityConfiguration.java
@@ -6,13 +6,22 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
   @Bean
   public DefaultSecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers("/").permitAll().anyRequest().authenticated()).oauth2Login(Customizer.withDefaults());
+    SimpleUrlAuthenticationSuccessHandler successHandler = new SimpleUrlAuthenticationSuccessHandler();
+    successHandler.setDefaultTargetUrl("/"); // Redirect to the root URL after successful login
+
+    http
+        .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+            .requestMatchers("/").permitAll()
+            .anyRequest().authenticated())
+        .oauth2Login(oauth2 -> oauth2.successHandler(successHandler)); // Add success handler for OAuth2 login
+
 
     return http.build();
   }
