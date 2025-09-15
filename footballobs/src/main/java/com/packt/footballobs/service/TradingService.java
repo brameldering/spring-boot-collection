b4f01ef6c894b05d6a07021e29d06f58368e3fb6
Bram Eldering <bram.eldering@gmail.com>
2025-09-15 16:41:44 +0200

    Added extra logging with different loglevels to test the workings of setting the logging level using actuator endpoints

--- footballobs/src/main/java/com/packt/footballobs/service/TradingService.java
+++ footballobs/src/main/java/com/packt/footballobs/service/TradingService.java
@@ -1,5 +1,7 @@
 package com.packt.footballobs.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.availability.AvailabilityChangeEvent;
 import org.springframework.boot.availability.LivenessState;
 import org.springframework.context.ApplicationEventPublisher;
@@ -11,19 +13,26 @@ import java.util.Random;
 public class TradingService {
   private final ApplicationEventPublisher applicationEventPublisher;
 
+  private static final Logger logger = LoggerFactory.getLogger(TradingService.class);
+
   public TradingService(ApplicationEventPublisher applicationEventPublisher) {
     this.applicationEventPublisher = applicationEventPublisher;
   }
 
   private int getPendingOrders() {
+    logger.debug("Ensuring that pending orders can be calculated");
     Random random = new Random();
-    return random.nextInt(100);
+    int pendingOrders = random.nextInt(100);
+    logger.info(pendingOrders + " pending orders found");
+    return pendingOrders;
   }
 
   public int tradeCards(int orders) {
     if (getPendingOrders() > 90) {
+      logger.warn("There are more than 90 orders, this can cause the system to crash");
       AvailabilityChangeEvent.publish(applicationEventPublisher, new Exception("There are more than 90 pending orders"), LivenessState.BROKEN);
     } else {
+      logger.debug("There are less than 90 orders, can manage it");
       AvailabilityChangeEvent.publish(applicationEventPublisher, new Exception("working fine"), LivenessState.CORRECT);
     }
     return orders;
